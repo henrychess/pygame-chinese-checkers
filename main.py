f17ca742b30e022a491100fc342922ee533d61f1
henrychess <91649568+henrychess@users.noreply.github.com>
2023-10-27 23:41:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 from game_logic.loops import *
 from game_logic.game import *
 from game_logic.player import *
-import pygame, sys
-from pygame.locals import *
-
+from game_logic.literals import *
+import pygame
 
 pygame.init()
-window = pygame.display.set_mode((800,600), pygame.SCALED | pygame.SRCALPHA)
+window = pygame.display.set_mode((WIDTH, HEIGHT), pygame.SCALED | pygame.SRCALPHA)
 pygame.display.set_caption('Chinese Checkers')
 
 lc = LoopController()
 
 while True:
-    """ for event in pygame.event.get():
+    """
+    for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit() """
