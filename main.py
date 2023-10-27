6d7584adf8685285ed3c6b638735c01a4442359e
henrychess <91649568+henrychess@users.noreply.github.com>
2023-10-27 13:54:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,20 +4,16 @@ from game_logic.player import *
 import pygame, sys
 from pygame.locals import *
 
+
 pygame.init()
 window = pygame.display.set_mode((800,600), pygame.SCALED | pygame.SRCALPHA)
 pygame.display.set_caption('Chinese Checkers')
 
-#set up the game
-g = Game(2)
-players = [HumanPlayer(1), Greedy2BotPlayer(2)]
+lc = LoopController()
 
 while True:
-    for event in pygame.event.get():
+    """ for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
-            sys.exit()
-    #mainMenuLoop(window)
-    winnerList, replayRecord = gameplayLoop(g, players, window)
-    #a = gameOverLoop(window, winnerList, replayRecord)
-    #if a == 1: mainMenuLoop(window)
+            sys.exit() """
+    lc.mainLoop(window)
