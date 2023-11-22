dcac649d1a89df4ab7208b01e856f87000817529
henrychess <91649568+henrychess@users.noreply.github.com>
2023-11-22 15:49:38 +0800

    Update CustomBotTemplate.py

--- custom_bots/CustomBotTemplate.py
+++ custom_bots/CustomBotTemplate.py
@@ -6,30 +6,6 @@ class CustomBotTemplate(Player):
         super().__init__()
     
     def pickMove(self, g:Game):
-        """
-            The following three functions grab stuff
-        that may be helpful for your bot to choose
-        a move.
-            Remember, all the functions return in
-        *subjective* coordinates. It doesn't matter
-        if you go first, second, or third.
-            The first one, allMovesDict, returns a
-        dictionary with the coordinates of each piece
-        as key, and the valid destination coordinates
-        a piece can go to as value.
-            The second one, getBoardState, returns the 
-        board state in dictionary form: the key is each
-        coordinate on the board, and the value is either
-        0, 1, 2, or 3: 0 means the spot is vacant,
-        otherwise it's the occupying piece's playerNum.
-            The third one, getBoolBoardState, is similar
-        to the second one. However, it only uses False
-        and True as value. False means the spot is vacant,
-        while True means it is occupied.
-            When you return the coordinates, remember to 
-        run the coordinates through subj_to_obj_coor(), 
-        like in the example below.
-        """
         moves = g.allMovesDict(self.playerNum)
         #board_state = g.getBoardState(self.playerNum)
         #bool_board_state = g.getBoolBoardState(self.playerNum)
