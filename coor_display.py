b14c8c0a5a47e51f6882400be1ac316b746733d3
henrychess <91649568+henrychess@users.noreply.github.com>
2023-11-23 11:27:47 +0800

    Create coor_display.py

---
+++ coor_display.py
@@ -0,0 +1,32 @@
+from game_logic.game import *
+from game_logic.helpers import *
+from game_logic.literals import *
+import pygame
+
+pygame.init()
+window = pygame.display.set_mode((WIDTH, HEIGHT), pygame.SCALED | pygame.RESIZABLE)
+pygame.display.set_caption('Chinese Checkers Coordinates')
+g = Game(3)
+window.fill(WHITE)
+g.drawPolygons(window)
+g.drawLines(window)
+for obj_coor in g.board:
+    coor = obj_to_subj_coor(obj_coor, playerNum)
+    c = add(g.centerCoor, mult(h2c(coor),g.unitLength)) #absolute coordinates on screen
+    pygame.draw.circle(window, WHITE, c, g.circleRadius)
+    pygame.draw.circle(window, BLACK, c, g.circleRadius, g.lineWidth)
+    if isinstance(g.board[obj_coor], Piece):
+        pygame.draw.circle(window, PLAYER_COLORS[g.board[obj_coor].getPlayerNum()-1], c, g.circleRadius-2)
+    coor_str = str(coor)
+    text = pygame.font.Font(size=14).render(coor_str, True, BLACK, None)
+    textRect = text.get_rect()
+    textRect.center = c
+    window.blit(text, textRect)
+pygame.display.update()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+    
